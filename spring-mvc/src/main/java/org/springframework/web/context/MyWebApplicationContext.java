1005af6a323b6ca7400ae3dce4168802055cf37e
zwd-admin <810095178@qq.com>
2019-05-24 11:06:55 +0800

    zwd

---
+++ spring-mvc/src/main/java/org/springframework/web/context/MyWebApplicationContext.java
@@ -0,0 +1,12 @@
+package org.springframework.web.context;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * TODO...
+ *
+ * @author zwd
+ * @since 2019-05-24
+ **/
+public interface MyWebApplicationContext extends ApplicationContext {
+}
